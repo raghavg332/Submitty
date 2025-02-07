50583ef860f13e04d74fe83e68d2e6c52ab23ed4
Raghav Gupta <raghav332004college@gmail.com>
2025-02-07 17:18:55 +0800

    fixing issues found in cypress

--- site/public/js/pdf/PDFAnnotateEmbedded.js
+++ site/public/js/pdf/PDFAnnotateEmbedded.js
@@ -234,13 +234,8 @@ function render(gradeable_id, user_id, grader_id, file_name, file_path, page_num
 
                 let pdfData;
                 try {
-                    pdfData = JSON.parse(data);
-                    // Checking if the response is a failure due to large file size
-                    if (pdfData.status === 'fail') {
-                        alert(pdfData.message);
-                        return;
-                    }
-                    pdfData = atob(pdfData['data']);
+                    pdfData = JSON.parse(data)['data'];
+                    pdfData = atob(pdfData);
                 }
                 catch (err) {
                     console.log(err);
