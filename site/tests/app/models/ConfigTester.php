8e5738fb0448b4d7757af5bc1b6c18cbcc12f7f0
Raghav Gupta <raghav332004college@gmail.com>
2025-02-19 19:54:29 +0800

    testtt

--- site/tests/app/models/ConfigTester.php
+++ site/tests/app/models/ConfigTester.php
@@ -96,6 +96,7 @@ class ConfigTester extends \PHPUnit\Framework\TestCase {
             'system_message' => "Some system message",
             "duck_special_effects" => false,
             "default_locale" => "default",
+            "course_material_file_upload_limit_mb" => 100,
         ];
         $config = array_replace($config, $extra);
         FileUtils::writeJsonFile(FileUtils::joinPaths($this->config_path, "submitty.json"), $config);
